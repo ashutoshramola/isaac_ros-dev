2b97f7ef6a4e56d3a6e7d1b53f30270e29e08633
ashutoshramola <ashutosh.ramola@outlook.com>
2024-07-15 16:08:19 +0530

    Updated px4_vslam package with latest from /home/ashutosh/packages/isaac_ros-dev/src

--- src/px4_vslam/launch/vslam_realsense.launch.py
+++ src/px4_vslam/launch/vslam_realsense.launch.py
@@ -23,26 +23,26 @@ from launch_ros.descriptions import ComposableNode
 def generate_launch_description():
 
     # Realsense node
-    realsense_camera_node = Node(
-        name='camera',
-        namespace='camera',
-        package='realsense2_camera',
-        executable='realsense2_camera_node',
-        parameters=[{
-                'enable_infra1': True,
-                'enable_infra2': True,
-                'enable_color': True,
-                'rgb_camera.profile': '640x360x30',
-                'enable_depth': False,
-                'depth_module.emitter_enabled': 0,
-                'depth_module.profile': '640x360x90',
-                'enable_gyro': False,
-                'enable_accel': False,
-                # 'gyro_fps': 200,
-                # 'accel_fps': 200,
-                # 'unite_imu_method': 2
-        }]
-    )
+    # realsense_camera_node = Node(
+    #     name='camera',
+    #     namespace='camera',
+    #     package='realsense2_camera',
+    #     executable='realsense2_camera_node',
+    #     parameters=[{
+    #             'enable_infra1': True,
+    #             'enable_infra2': True,
+    #             'enable_color': True,
+    #             'rgb_camera.profile': '640x360x30',
+    #             'enable_depth': False,
+    #             'depth_module.emitter_enabled': 0,
+    #             'depth_module.profile': '640x360x90',
+    #             'enable_gyro': False,
+    #             'enable_accel': False,
+    #             # 'gyro_fps': 200,
+    #             # 'accel_fps': 200,
+    #             # 'unite_imu_method': 2
+    #     }]
+    # )
 
     # Static transform publisher PX4 to ROS2
     # Apply ROLL_180 for NED to FLU
@@ -74,74 +74,74 @@ def generate_launch_description():
     )
 
     # Isaac ROS VSLAM
-    visual_slam_node = ComposableNode(
-        name='visual_slam_node',
-        package='isaac_ros_visual_slam',
-        plugin='isaac_ros::visual_slam::VisualSlamNode',
-        parameters=[{
-                    'denoise_input_images': False,
-                    'rectified_images': True,
-                    'enable_debug_mode': False,
-                    'debug_dump_path': '/tmp/cuvslam',
-                    'enable_slam_visualization': True,
-                    'enable_landmarks_view': True,
-                    'enable_observations_view': True,
-                    'map_frame': 'map',
-                    'odom_frame': 'odom',
-                    'base_frame': 'camera_link',
-                    'input_imu_frame': 'camera_gyro_optical_frame',
-                    'enable_imu_fusion': True,
-                    'gyro_noise_density': 0.001,
-                    'gyro_random_walk': 0.000019393,
-                    'accel_noise_density': 0.003,
-                    'accel_random_walk': 0.003,
-                    'calibration_frequency': 100.0,
-                    'img_jitter_threshold_ms': 22.00
-                    }],
-        remappings=[('stereo_camera/left/image', 'camera/infra1/image_rect_raw'),
-                    ('stereo_camera/left/camera_info', 'camera/infra1/camera_info'),
-                    ('stereo_camera/right/image', 'camera/infra2/image_rect_raw'),
-                    ('stereo_camera/right/camera_info', 'camera/infra2/camera_info'),
-                    ('visual_slam/imu', 'vio_transform/imu')]
-    )
+    # visual_slam_node = ComposableNode(
+    #     name='visual_slam_node',
+    #     package='isaac_ros_visual_slam',
+    #     plugin='isaac_ros::visual_slam::VisualSlamNode',
+    #     parameters=[{
+    #                 'denoise_input_images': False,
+    #                 'rectified_images': True,
+    #                 'enable_debug_mode': False,
+    #                 'debug_dump_path': '/tmp/cuvslam',
+    #                 'enable_slam_visualization': True,
+    #                 'enable_landmarks_view': True,
+    #                 'enable_observations_view': True,
+    #                 'map_frame': 'map',
+    #                 'odom_frame': 'odom',
+    #                 'base_frame': 'camera_link',
+    #                 'input_imu_frame': 'camera_gyro_optical_frame',
+    #                 'enable_imu_fusion': True,
+    #                 'gyro_noise_density': 0.001,
+    #                 'gyro_random_walk': 0.000019393,
+    #                 'accel_noise_density': 0.003,
+    #                 'accel_random_walk': 0.003,
+    #                 'calibration_frequency': 100.0,
+    #                 'img_jitter_threshold_ms': 22.00
+    #                 }],
+    #     remappings=[('stereo_camera/left/image', 'camera/infra1/image_rect_raw'),
+    #                 ('stereo_camera/left/camera_info', 'camera/infra1/camera_info'),
+    #                 ('stereo_camera/right/image', 'camera/infra2/image_rect_raw'),
+    #                 ('stereo_camera/right/camera_info', 'camera/infra2/camera_info'),
+    #                 ('visual_slam/imu', 'vio_transform/imu')]
+    # )
 
     # Foxglove
-    foxglove_bridge_node = Node(
-        name='foxglove_bridge',
-        namespace='foxglove_bridge',
-        package='foxglove_bridge',
-        executable='foxglove_bridge',
-        parameters=[{
-                    'port': 8765,
-                    'address': '0.0.0.0',
-                    'tls': False,
-                    'certfile': '',
-                    'keyfile': '',
-                    'topic_whitelist': ['.*'],
-                    'service_whitelist': ['.*'],
-                    'param_whitelist': ['.*'],
-                    'client_topic_whitelist': ['.*'],
-                    'min_qos_depth': 1,
-                    'max_qos_depth': 10,
-                    'num_threads': 0,
-                    'send_buffer_limit': 10000000,
-                    'use_sim_time': False,
-                    'capabilities': ['clientPublish','parameters','parametersSubscribe','services','connectionGraph','assets'],
-                    'include_hidden': False,
-                    'asset_uri_allowlist': ['package://(\\w+/?)+\\.(dae|stl|urdf|xacro)']
-                    }]
-    )
+    # foxglove_bridge_node = Node(
+    #     name='foxglove_bridge',
+    #     namespace='foxglove_bridge',
+    #     package='foxglove_bridge',
+    #     executable='foxglove_bridge',
+    #     parameters=[{
+    #                 'port': 8765,
+    #                 'address': '0.0.0.0',
+    #                 'tls': False,
+    #                 'certfile': '',
+    #                 'keyfile': '',
+    #                 'topic_whitelist': ['.*'],
+    #                 'service_whitelist': ['.*'],
+    #                 'param_whitelist': ['.*'],
+    #                 'client_topic_whitelist': ['.*'],
+    #                 'min_qos_depth': 1,
+    #                 'max_qos_depth': 10,
+    #                 'num_threads': 0,
+    #                 'send_buffer_limit': 10000000,
+    #                 'use_sim_time': False,
+    #                 'capabilities': ['clientPublish','parameters','parametersSubscribe','services','connectionGraph','assets'],
+    #                 'include_hidden': False,
+    #                 'asset_uri_allowlist': ['package://(\\w+/?)+\\.(dae|stl|urdf|xacro)']
+    #                 }]
+    # )
 
     # Launch all nodes in the same process
-    visual_slam_launch_container = ComposableNodeContainer(
-        name='visual_slam_launch_container',
-        namespace='',
-        package='rclcpp_components',
-        executable='component_container',
-        composable_node_descriptions=[
-            visual_slam_node
-        ],
-        output='screen'
-    )
+    # visual_slam_launch_container = ComposableNodeContainer(
+    #     name='visual_slam_launch_container',
+    #     namespace='',
+    #     package='rclcpp_components',
+    #     executable='component_container',
+    #     composable_node_descriptions=[
+    #         visual_slam_node
+    #     ],
+    #     output='screen'
+    # )
     # return launch.LaunchDescription([visual_slam_launch_container, realsense_camera_node, imu_transform_node, vio_transform_node, foxglove_bridge_node])
-    return launch.LaunchDescription([visual_slam_launch_container, realsense_camera_node, camera_link_gyro_tf_node, gyro_optical_tf_node, vio_transform_node, foxglove_bridge_node])
+    return launch.LaunchDescription([camera_link_gyro_tf_node, gyro_optical_tf_node, vio_transform_node])
